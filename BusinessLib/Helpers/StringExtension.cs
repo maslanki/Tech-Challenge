7ec1c31526d8ded08b28df8cb762ab744513a025
maslanki <aleksandramaslankiewicz0@gmail.com>
2022-07-29 12:01:56 +0200

    toString extension

---
+++ BusinessLib/Helpers/StringExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.Text;
+namespace BusinessLib.Helpers
+{
+    public static class DummyStringHelper
+    {
+        public static string ToString(this object o)
+        {
+            PropertyDescriptorCollection coll = TypeDescriptor.GetProperties(o);
+            StringBuilder builder = new StringBuilder();
+            foreach (PropertyDescriptor pd in coll)
+            {
+                if (pd.GetValue(o) == null) continue;
+                builder.Append(string.Format("{0} : {1}", pd.Name, pd.GetValue(o).ToString()));
+                builder.Append("\n");
+
+            }
+            return builder.ToString();
+        }
+    }
+}
