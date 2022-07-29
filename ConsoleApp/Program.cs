5edb95806126d7016502dd0facae15ec6295ac17
maslanki <aleksandramaslankiewicz0@gmail.com>
2022-07-29 10:11:04 +0200

    init

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using BusinessLib;
+
+Console.WriteLine("Hello, World!");
+var x = new Class1();
+var res = await x.Get();
+
+Console.WriteLine(res);
