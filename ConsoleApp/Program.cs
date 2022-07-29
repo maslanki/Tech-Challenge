4fd8057d09a93f5c17d9600ae685721f73912f52
maslanki <aleksandramaslankiewicz0@gmail.com>
2022-07-29 15:05:18 +0200

    console app update

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -11,3 +11,8 @@ inProgressOrders.ForEach(o => Console.WriteLine(o));
 Console.WriteLine("\n\n\n****** Top 5 products sold ******");
 var topProducts = await orderManager.GetTop5ProductsAsync(inProgressOrders);
 topProducts.ForEach(o => Console.WriteLine(o));
+
+var firstProductId = topProducts.FirstOrDefault().MerchantProductNo;
+Console.WriteLine($"****** Setting a stock = 25 of the product with merchantProductNo = {firstProductId} ******");
+await orderManager.SetStock(firstProductId);
+Console.WriteLine($"Done!");
