dd7b07c564c2204c368353a18b07902ce23cff5b
maslanki <aleksandramaslankiewicz0@gmail.com>
2022-07-29 12:45:23 +0200

    methods name

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,5 +9,5 @@ var inProgressOrders = await orderManager.GetInProgressOrdersAsync();
 inProgressOrders.ForEach(o => Console.WriteLine(o));
 
 Console.WriteLine("\n\n\n****** Top 5 products sold ******");
-var topProducts = await orderManager.GetTop5Products(inProgressOrders);
+var topProducts = await orderManager.GetTop5ProductsAsync(inProgressOrders);
 topProducts.ForEach(o => Console.WriteLine(o));
