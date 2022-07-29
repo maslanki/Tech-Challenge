c363e086b246956e982348e9201d67196571c983
maslanki <aleksandramaslankiewicz0@gmail.com>
2022-07-29 11:27:27 +0200

    fitlering top 5 products + manager code clean up

---
+++ BusinessLib/Managers/OrderManager.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLib.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Json;
+
+namespace BusinessLib.Managers
+{
+    public class OrderManager
+    {
+        static string apikey = "541b989ef78ccb1bad630ea5b85c6ebff9ca3322";
+        static string address = $"https://api-dev.channelengine.net/api/v2/orders?apikey={apikey}&statuses=IN_PROGRESS";
+
+        public async Task<List<Order>> GetInProgressOrdersAsync()
+        {
+            var client = new HttpClient();
+            var result = await client.GetFromJsonAsync<RequestResult>(address);
+            
+            if (result == null) return new List<Order>();
+
+            return result.Content;
+        }
+
+        public async Task<List<Product>> GetTop5Products()
+        {
+            var orders = await GetInProgressOrdersAsync();
+
+            //I assume GTIN is an ID of the product (?)
+            //what about description? same GTIN gives different descriptions
+
+            var products = orders
+                .SelectMany(o => o.Lines)
+                .GroupBy(l => l.Gtin)
+                .Select(g => new Product
+                {
+                    Gtin = g.Key,
+                    ProductName = g.FirstOrDefault().Description,
+                    TotalQuantity = g.Sum(c => c.Quantity)
+                })
+                .OrderByDescending(g => g.TotalQuantity);
+
+            return products.Take(5).ToList();
+        }
+    }
+
+}
